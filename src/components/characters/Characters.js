8d4b269cefe41c2ddbe58225775ebdd27a52591f
Jonathan Forney <jonathan.forney@gmail.com>
2019-09-10 16:34:01 -0700

    character and characters components plus css

---
+++ src/components/characters/Characters.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Character from './Character';
+import styles from './Characters.css';
+
+function Characters({ characters }) {
+  //eslint-desable-next-line
+  const characterList = characters.map((character, i) => ( //eslint-disable-line react/prop-types
+
+    <li key={i}>
+      <Character character={character} />
+    </li>
+  ));
+
+  return (
+    <ul className={styles.Characters}>
+      {characterList}
+    </ul>
+  );
+}
+
+Character.propTypes = {
+  characters: PropTypes.array.isRequired,
+};
+
+export default Characters;
